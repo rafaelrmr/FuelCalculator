dda689787ad957f5c2b1d468344aa0d1883a15e5
Rafael <rafael.mota8@gmail.com>
2024-10-10 21:20:53 +0100

    -Tela Inicial criada, configuraçao do botão para chamada da próxima tela -1ª Tela de entrada de dados

--- app/src/main/java/com/example/fuelcalculator/activity_preco.kt
+++ app/src/main/java/com/example/fuelcalculator/activity_preco.kt
@@ -1,28 +1,19 @@
 package com.example.fuelcalculator
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class activity_preco : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_preco)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnIniciar = findViewById<Button>(R.id.btn_iniciar)
-
-        btnIniciar.setOnClickListener {
-            val intent = Intent(this,activity_preco::class.java)
-            startActivity(intent)
         }
     }
 }
