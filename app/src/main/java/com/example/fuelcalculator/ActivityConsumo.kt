d54d362c0ab11fa6f277ac0ca85eeb06a2f82066
Rafael <rafael.mota8@gmail.com>
2024-10-18 00:47:37 +0100

    - Criada segunda tela de inserção de dados - Configurada a ação do botão para chamar a próxima tela

--- app/src/main/java/com/example/fuelcalculator/ActivityConsumo.kt
+++ app/src/main/java/com/example/fuelcalculator/ActivityConsumo.kt
@@ -1,28 +1,19 @@
 package com.example.fuelcalculator
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_preco : AppCompatActivity() {
+class ActivityConsumo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preco)
+        setContentView(R.layout.activity_consumo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnProximo = findViewById<Button>(R.id.btn_proximo)
-
-            btnProximo.setOnClickListener {
-                val intent = Intent (this, ActivityConsumo :: class.java)
-                startActivity(intent)
-            }
     }
 }
